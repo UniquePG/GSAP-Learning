8e2ca992d614f24901c88483f0b9b5b010ba846d
= <=>
2024-09-21 15:16:11 +0530

    scroll trigger finised 21-09-2024

--- Scroll-Trigger/script.js
+++ Scroll-Trigger/script.js
@@ -1,28 +1,101 @@
 
-gsap.from("#page1 #box", {
-    opacity: 0,
-    scale: 0,
-    duration: 2,
-    delay: 1,
-    rotate: 360
-})
+// gsap.from("#page1 #box", {
+//     opacity: 0,
+//     scale: 0,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360
+// })
 
-gsap.from("#page2 #box", {
-    opacity: 0,
-    scale: 0,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
 
-    // scrollTrigger: "#page2 #box"
+// gsap.from("#page2 #box", {
+//     opacity: 0,
+//     scale: 0,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+
+//     // scrollTrigger: "#page2 #box"
+
+//     scrollTrigger: {
+//         trigger: "#page2 #box",             // animation ka starting point
+//         scroller: "body",                   // mostly we write body
+
+//         markers: true,              
+//         start: "top 60%"
+//     }
+
+// })
+
+
+
+// gsap.from("#page3 h1", {
+//     opacity: 0,
+//     duration: 2,
+//     x: 600,
+
+//     scrollTrigger:{
+//         trigger: "#page3 h1",
+//         scroller: "body",
+
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+// gsap.from("#page3 h2", {
+//     opacity: 0,
+//     duration: 2,
+//     x: -600,
+
+//     scrollTrigger:{
+//         trigger: "#page3 h2",
+//         scroller: "body",
+
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+
+////!---------------- scrub -> animation dependent on scrolling
+
+// gsap.from("#page2 #box", {
+//     opacity: 0,
+//     scale: 0,
+//     rotate: 720,
+//     duration: 1,
+
+//     scrollTrigger: {
+//         trigger: "#page2 #box",
+//         scroller: "body",
+//         markers: true,
+
+//         start: "top 60%",       // starting point of trigger animation
+//         end: "top 30%",          // end point of animation
+
+//         scrub: 2               // animation happens based on scrolling  ( it should be true or 1 to 5 (for smoothness))
+//     }
+// })
+
+
+
+
+///!---------------- pin -> pin the parent while animation
+    //*  if we want to use pin property then we have to always trigger the parent of the animation trigger element
+
+gsap.to("#page2 h1", {
+
+    transform: "translateX(-175%)",
 
     scrollTrigger: {
-        trigger: "#page2 #box",
+        trigger: "#page2",  // we have to define the parent in trigger (page2 is the parent of the h1)
         scroller: "body",
-
         markers: true,
-        start: "top 60%"
+        start: "top 0%",
+        end: "top -100%",       // -100% means outside the whole body of the browser
+        scrub: 2,
+        pin: true       // enable pin (untill all the animation finish we cant scroll to the next page)
+
     }
-
 })
-
--- Scroll-Trigger/scrolltrigger.md
+++ Scroll-Trigger/scrolltrigger.md
@@ -1,6 +1,21 @@
 
 # scroll Trigger  -> animation start when specific scroll hit
 
+    // scroll trigger 1st method
     scrollTrigger: "html element"
 
-    
+///-------
+
+        // scroll trigger object      
+     scrollTrigger: {   
+        trigger: "#page2 #box",             // animation ka starting point
+        scroller: "body",                   // mostly we write body
+
+        markers: true,                      // to help us with start and end markers
+        start: "top 60%"                    // trigger point line (top se 60% par scroll ka marker)
+
+        scrub: true  || (1 to 5 -> for smoothness)   // animation trigger on scrolling
+
+        pin: true    // -> to use this pin we always have to define the parent(of animation element) in the trigger
+            // it freeshed that perticular area until all animation is finished   
+    }
